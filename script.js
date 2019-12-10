5d5e567469fb2f3c4e227b5f2c24445e410e3287
Andy <djusha817@gmail.com>
2019-12-10 15:59:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//a = 5;
+//console.log(a);
+//var leftBorderWidth = 1;
+//let second = 2;
+//const pi  = 3.14;
+//let sumthing;
+
+console.log(4/0);
+console.log('string'*9);
+console.log(sumthing);
